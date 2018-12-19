bb0ed6f44cd39906c5b664adfa9a38188b26f497
JVB11 <45052356+JVB11@users.noreply.github.com>
2018-12-19 18:45:17 +0100

    License + utility added

--- create_data_Blazhko_csv.py
+++ create_data_Blazhko_csv.py
@@ -6,16 +6,35 @@
 
 # Description: Generates the text file for the main dataframe of the main script (Has to be generated because the main script is still written in python 2.7...
 
+#   Copyright 2018 Jordan Van Beeck
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#       http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 from astroquery.vizier import Vizier
 import pandas as pd
 import re
 import numpy as np
 
-Vizier.ROW_LIMIT = -1 # unlimited row limit for query
+#----------------------------------------------------------------------
+# Specify the name of the saved file:
+Savedfilename = 'BLAZHKO_csv.dat'
+# Specify the paths of files needed to generate saved file:
+csv_path = '~/Documents/GAIA/'
+csv_gaia_id = "Xmatch.csv"
+csv_filename = "RRL.csv"
+#----------------------------------------------------------------------
 
-csv_path = '/Users/jvb/Documents/Proposal2018/GAIA/'
-csv_gaia_id = "DambisGaiaXmatch.csv"
-csv_filename = "RRL_Dambis2013.csv"
+Vizier.ROW_LIMIT = -1 # unlimited row limit for query
 
 # load csv with stars and respective useful columns, then drop any rows containing NaNs in them
 data_csv = pd.read_csv(csv_path+csv_filename)
@@ -23,8 +42,6 @@ data_csv = data_csv[["Name","Period","FeH","RRtype","Vmag","e_Vmag","Kmag","e_Km
 data_csv = data_csv.dropna(axis=0)
 # get all the starnames
 starnames = data_csv["Name"].tolist()
-
-
 
 blazhkos = []
 starnames_blazhkos = []
@@ -58,4 +75,4 @@ headertxt = ["Starname","Blazhko"]
 headerstring = '\t'.join(headertxt)
 
 # save the GAIA PARALLAXES to a tab-delimited .dat-file, in order to be read in by the main script
-np.savetxt('BLAZHKO_csv.dat',np.vstack((starnames_blazhkos,blazhkos)).T,delimiter='\t',fmt="%s",header=headerstring)
+np.savetxt(Savedfilename,np.vstack((starnames_blazhkos,blazhkos)).T,delimiter='\t',fmt="%s",header=headerstring)
